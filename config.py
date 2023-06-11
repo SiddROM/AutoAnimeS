3ce7964609ff84102d2988b4f8bb45877af1113f
SiddROM <121415948+SiddROM@users.noreply.github.com>
2023-06-11 15:27:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,18 +6,18 @@ if os.path.exists("config.env"):
 elif os.path.exists("sample.env"):
     load_dotenv("sample.env")
 
-API_ID = os.getenv("API_ID")
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-MONGO_DB_URI = os.getenv("MONGO_DB_URI")
-STATUS_MSG_ID = os.getenv("STATUS_MSG_ID")
-SCHEDULE_MSG_ID = os.getenv("SCHEDULE_MSG_ID")
-CHANNEL_TITLE = os.getenv("CHANNEL_TITLE", "AnimeDex")
-INDEX_CHANNEL_USERNAME = os.getenv("INDEX_CHANNEL_USERNAME")
-UPLOADS_CHANNEL_USERNAME = os.getenv("UPLOADS_CHANNEL_USERNAME")
-TECHZ_API_KEY = os.getenv("TECHZ_API_KEY")
-COMMENTS_GROUP_LINK = os.getenv("COMMENTS_GROUP_LINK")
-SLEEP_TIME = os.getenv("SLEEP_TIME", 60)
+API_ID = os.getenv("26019444")
+API_HASH = os.getenv("a308fac723905cdbd836414b18f3b3d6")
+BOT_TOKEN = os.getenv("6100344970:AAGrq-OnnEwz9KBahxtCcdo68pXG7kV6Z1I")
+MONGO_DB_URI = os.getenv("mongodb+srv://xenfilebot:xenfilebot@cluster0.sskycsa.mongodb.net/?retryWrites=true&w=majority")
+STATUS_MSG_ID = os.getenv("573")
+SCHEDULE_MSG_ID = os.getenv("574")
+CHANNEL_TITLE = os.getenv("CHANNEL_TITLE", "REALM OF WEEBS")
+INDEX_CHANNEL_USERNAME = os.getenv("OngoingAnimeIndex")
+UPLOADS_CHANNEL_USERNAME = os.getenv("OngoingAnimeRealms")
+TECHZ_API_KEY = os.getenv("GUUOXT")
+COMMENTS_GROUP_LINK = os.getenv("https://t.me/+KUxfqDGbQj0zMTM1")
+SLEEP_TIME = os.getenv("15", 60)
 
 for k, v in {
     "API_ID": API_ID,
